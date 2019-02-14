fd14aa6176d0069dd50be807da5ae20278ffbda3
Chayan Datta <sonicxxx7@gmail.com>
2019-02-14 12:06:16 +0530

    User button alter

--- resources/views/views/events.blade.php
+++ resources/views/views/events.blade.php
@@ -624,7 +624,7 @@
                     <span>Register</span>
                 </div>
         
-                <form class="" method="POST" id="registrationform" action="/registerEvent">
+                <form class="contact100-form" method="POST" id="registrationform" action="/registerEvent">
                     @csrf
                     <div class="wrap-input100 validate-input">
                         <input id="name" class="input100" type="text" name="team" placeholder="Team Name" autocomplete="off" required>
@@ -683,7 +683,7 @@
                     <button id="delmember" class="btn btn-danger btn-sm" type="button">-</button>
         
                     <div class="container-contact100-form-btn">
-                        <input type="submit" class="btn-success btn-block" value="Submit">
+                        <input type="submit" class="btn-block" value="Submit">
                     </div>
                 </form>
             </div>
