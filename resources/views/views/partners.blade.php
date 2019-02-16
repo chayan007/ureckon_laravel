69b14f90d01dcc29718898ef0a133e258b00b612
SkiddieSayak <sayak@live.in>
2019-02-17 00:16:03 +0530

    update

--- resources/views/views/partners.blade.php
+++ resources/views/views/partners.blade.php
@@ -53,66 +53,66 @@
         <!-- start of 1st row -->
         <div style="text-align: center" class="row">
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="../images/partners/event_partners/devc.png" onmouseover="gray(this)" onmouseout="nogray(this)" alt="" width="200" height="200">
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/devc.png')}}" onmouseover="gray(this)" onmouseout="nogray(this)" alt="" width="200" height="200">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="../images/partners/event_partners/gdg.png" onmouseover="gray(this)" onmouseout="nogray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/gdg.png')}}" onmouseover="gray(this)" onmouseout="nogray(this)"
               alt="" width="300" height="300">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="../images/partners/event_partners/wtm.png" onmouseover="gray(this)" onmouseout="nogray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/wtm.png')}}" onmouseover="gray(this)" onmouseout="nogray(this)"
               alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="../images/partners/event_partners/girlsScript.jpg" onmouseover="gray(this)" onmouseout="nogray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/girlsScript.jpg')}}" onmouseover="gray(this)" onmouseout="nogray(this)"
               alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="../images/partners/event_partners/intel.png" onmouseover="gray(this)" onmouseout="nogray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/intel.png')}}" onmouseover="gray(this)" onmouseout="nogray(this)"
               alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="../images/partners/event_partners/krs.jpeg" onmouseover="gray(this)" onmouseout="nogray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/krs.jpeg')}}" onmouseover="gray(this)" onmouseout="nogray(this)"
               alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="https://source.unsplash.com/random" onmouseover="gray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/tcf logo.png')}}" onmouseover="gray(this)"
               onmouseout="nogray(this)" alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="https://source.unsplash.com/random" onmouseover="gray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/skubotics.png')}}" onmouseover="gray(this)"
               onmouseout="nogray(this)" alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="https://source.unsplash.com/random" onmouseover="gray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/codechef.png')}}" onmouseover="gray(this)"
               onmouseout="nogray(this)" alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="https://source.unsplash.com/random" onmouseover="gray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/csi-logo.png')}}" onmouseover="gray(this)"
               onmouseout="nogray(this)" alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="https://source.unsplash.com/random" onmouseover="gray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/acm_chapter_uemkol_1.png')}}" onmouseover="gray(this)"
               onmouseout="nogray(this)" alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="https://source.unsplash.com/random" onmouseover="gray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/ieee_uemk_sb.png')}}" onmouseover="gray(this)"
               onmouseout="nogray(this)" alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="https://source.unsplash.com/random" onmouseover="gray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/ecell_logo_new_png.png')}}" onmouseover="gray(this)"
               onmouseout="nogray(this)" alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="https://source.unsplash.com/random" onmouseover="gray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/IIC_LOGO.png')}}" onmouseover="gray(this)"
               onmouseout="nogray(this)" alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="https://source.unsplash.com/random" onmouseover="gray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/photo.jpg')}}" onmouseover="gray(this)"
               onmouseout="nogray(this)" alt="" width="150" height="150">
           </div>
           <div class="col-md-4">
-            <img class="mt-4 img-fluid partner_logo" src="https://source.unsplash.com/random" onmouseover="gray(this)"
+            <img class="mt-4 img-fluid partner_logo" src="{{ asset('images/partners/event_partners/SPIE UEMK 20180719_215336.jpg')}}" onmouseover="gray(this)"
               onmouseout="nogray(this)" alt="" width="150" height="150">
           </div>
         </div>
