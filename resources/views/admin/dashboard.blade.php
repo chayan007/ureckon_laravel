dfc276cef10bb3ba9e3b83af11bd0e59b177c60c
Chayan Datta <sonicxxx7@gmail.com>
2019-02-17 14:49:30 +0530

    User Admin done

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,41 @@
+@extends('admin.layouts.admin')
+@section('page', 'Dashboard')
+@section('content')
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <strong class="card-title">List of Users</strong>
+            </div>
+            <div class="card-body">
+                <table id="bootstrap-data-table-export" class="table table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Serial</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>College</th>
+                        <th>Username</th>
+                        <th>Phone</th>
+                        <th>DP</th>
+                        <th>Course</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($users as $user)
+                        <tr>
+                            <td> {{ $loop->iteration }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->college }}</td>
+                            <td>{{ $user->username }}</td>
+                            <td>{{ $user->phone }}</td>
+                            <td><img src="{{ Storage::url($user->img_url) }}" alt="user"></td>
+                            <td>{{ $user->course }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/includes/panel.blade.php
@@ -0,0 +1,21 @@
+<aside id="left-panel" class="left-panel">
+    <nav class="navbar navbar-expand-sm navbar-default">
+
+        <div class="navbar-header">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="./">Ureckon</a>
+            <a class="navbar-brand hidden" href="./"><i class="fa fa-signal"></i></a>
+        </div>
+
+        <div id="main-menu" class="main-menu collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="/admin/users"> <i class="menu-icon fa fa-dashboard"></i>User </a>
+                    <a href="/admin/participants"> <i class="menu-icon fa fa-dashboard"></i>Participants </a>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</aside><!-- /#left-panel -->
