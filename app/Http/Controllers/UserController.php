ea0c61151532ac937ae4e987aa52acbd604c9e86
Chayan Datta <sonicxxx7@gmail.com>
2019-02-20 01:18:00 +0530

    Check

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,9 +14,10 @@ class UserController extends Controller
 
     public function events()
     {
-        $usernames = User::all()->values('username');
+        $usernames = User::all()->get('username');
         return view('views.events', ['usernames' => $usernames]);
     }
+
     public function profile()
     {
         if(Auth::check())
